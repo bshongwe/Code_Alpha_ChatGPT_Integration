659a0a96442eed4d112b4ed9602d388bd81c5ba2
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-06 21:08:57 +0200

    React & React Chat UI install

---
+++ app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
--- trash-prototype/app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-
-import { initializeWebSocket, sendMessage } from './websocket.js';
-
-// Call function to initialize WebSocket connection on page load
-initializeWebSocket();
-
-// Example usage of sendMessage function
-sendMessage("Hello, WebSocket ChatGPT!");
