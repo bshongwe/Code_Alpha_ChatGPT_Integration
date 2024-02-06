66f3dd7be41a7f967bc0b7909839ab68af5adf2c
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-06 21:22:03 +0200

    Creating Chat UI

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,12 +6,22 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
+ return (
+ <>
+   {/* A container for the chat window */}
+   <div style={{ position: "relative", height: "100vh", width: "700px" }}>
+     {/* All components are wrapped in the MainContainer */}
+     <MainContainer>
+       {/* All chat logic will be contained in the ChatContainer */}
+       <ChatContainer>
+         {/* Shows all our messages */}
+         <MessageList></MessageList>
+       </ChatContainer>
+     </MainContainer>
+   </div>
+ </>
+ );
 }
 
 export default App
